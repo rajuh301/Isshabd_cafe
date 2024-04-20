a27f2acba5d3c94e8e8e3dcbd1c0748989f0b067
Md. Nuruzzaman <rajuh301@gmail.com>
2024-04-20 15:56:28 +0600

    last

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,8 +13,8 @@ const roboto = Roboto({
 })
 
 export const metadata = {
-  title: "Easy-Shop",
-  description: "Easy-Shop is an e-commerce web application",
+  title: "Issha bd",
+  description: "Issha bd is an e-commerce web application",
 };
 
 export default function RootLayout({ children }) {
